7a344beb15732c1e511c101a37da52117a38c967
San120806 <saniyaakapure@gmail.com>
2025-12-11 13:15:02 +0530

    added a fallbck url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,   // <-- this is the fix
+  baseURL: import.meta.env.VITE_API_URL || "https://buildtrack-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
